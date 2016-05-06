d449b6144dc1e6ae5527519a75775dd14cc9a3d7
Salih <muhammedsalihguler@gmail.com>
2016-05-06 17:04:58 +0300

    Facebook Login Implementation

--- app/src/main/java/com/teamspeaghetti/www/gifster/MainActivity.java
+++ app/src/main/java/com/teamspeaghetti/www/gifster/MainActivity.java
@@ -1,13 +1,58 @@
 package com.teamspeaghetti.www.gifster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.login_page);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        callbackManager=CallbackManager.Factory.create();
+        AppEventsLogger.activateApp(this);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this,"Login Cancelled",Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(MainActivity.this,"Login Failed:"+error,Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
