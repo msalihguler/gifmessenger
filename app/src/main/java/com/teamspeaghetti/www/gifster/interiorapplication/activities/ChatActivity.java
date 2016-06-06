ae4544d5569d744405f8b63ac44f33cc7949623e
Salih <muhammedsalihguler@gmail.com>
2016-06-06 12:29:07 +0300

    Keyboard animation added

--- app/src/main/java/com/teamspeaghetti/www/gifster/interiorapplication/activities/ChatActivity.java
+++ app/src/main/java/com/teamspeaghetti/www/gifster/interiorapplication/activities/ChatActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.teamspeaghetti.www.gifster.R;
 
@@ -11,6 +16,9 @@ import com.teamspeaghetti.www.gifster.R;
  * Created by Salih on 6.06.2016.
  */
 public class ChatActivity extends AppCompatActivity {
+    Button toggle;
+    Boolean toogled=true;
+    LinearLayout keyboard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +28,27 @@ public class ChatActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         toolbar.setTitle(getIntent().getExtras().getString("name"));
+        final Animation slide_down = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide_down);
 
+        final Animation slide_up = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide_up);
+        slide_up.setFillAfter(true);
+        slide_down.setFillAfter(true);
+        keyboard = (LinearLayout)findViewById(R.id.keyboard);
+        toggle = (Button)findViewById(R.id.toggle);
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(toogled){
+                    keyboard.startAnimation(slide_down);
+                    toogled=false;
+                }else{
+                    keyboard.startAnimation(slide_up);
+                    toogled=true;
+                }
+            }
+        });
 
     }
 }
