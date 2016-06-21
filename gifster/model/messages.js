458cd59f7b7993b0ed8a1ebada614f0f628747c2
Salih <muhammedsalihguler@gmail.com>
2016-06-21 17:24:33 +0300

    Backend part seperated

--- gifster/model/messages.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var mongoSchema =   mongoose.Schema;
-
-var messages = {
-	"chat_id":String,
-	"sender_id":String,
-    "message":String
-}
-module.exports = mongoose.model('messages',messages);
--- gifster/model/reveals.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-var mongoSchema =   mongoose.Schema;
-
-var reveals = {
-	"userid":String,
-	"revealed_profiles":String
-}
-module.exports = mongoose.model('reveals',reveals);
